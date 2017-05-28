b475856704901311671be5eaf6f7c03b39ec9112
Nanofication <nansquared@gmail.com>
2017-05-28 09:29:55 -0400

    Added Sendfile, displays html file

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.sendFile(__dirname + '/index.html')
 })
 // Note: Request and response are usually written as req and res respectively
+
 
 app.listen(3000, function(){
   console.log('listening on 3000')
