78d7a290714255eb5501f51fdeee44011b73654d
Nanofication <nansquared@gmail.com>
2017-05-28 09:53:33 -0400

    Woot added MONGO DB!

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
@@ -7,7 +10,7 @@ app.get('/', (req, res) => {
 // Note: Request and response are usually written as req and res respectively
 
 app.post('/quotes', (req, res) => {
-  console.log("So this worked hahaha!")
+  console.log(req.body)
 })
 
 app.listen(3000, function(){
