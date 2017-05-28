73aa2da8d773507fe800d3d25a3252de7b033b6a
Nanofication <nansquared@gmail.com>
2017-05-28 09:23:48 -0400

    Send hello world request to server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+// Note: Request and response are usually written as req and res respectively
+
 app.listen(3000, function(){
   console.log('listening on 3000')
 })
